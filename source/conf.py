49ec4bd322c640bfa3d14dbe85ea50246231a635
Nathan Rees <100367103+NRees-Anaconda@users.noreply.github.com>
2024-05-18 16:08:38 -0700

    Initial commit

---
+++ source/conf.py
@@ -0,0 +1,41 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Nathan_Rees_Portfolio'
+copyright = '2024, Nathan Rees'
+author = 'Nathan Rees'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx_rtd_theme',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# Custom sidebar templates, must be a dictionary that maps document names to template names.
+html_logo = "Bird-writer.png"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ source/index.rst
@@ -0,0 +1,24 @@
+.. Nathan_Rees_Portfolio documentation master file, created by
+   sphinx-quickstart on Sat May 18 15:52:45 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+===============
+Hi, I'm Nathan!
+===============
+
+I write end user documentation tailored for every day people who need to navigate complex systems. This page is currently under construction. 
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+      about-me
+      portfolio 
+      hire-me
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
